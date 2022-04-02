716e9b6fb0c6917c3041c340a6221730b4ad26c8
s5255384 <100850996+s5255384@users.noreply.github.com>
2022-04-02 17:07:42 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,5 +2,24 @@
 using namespace std;
 
 int main() {
+        int i, n, a=1, b=1, sum=0;
+        cout<<"inserisci quante n volte sviluppare la serie di fibonacci."<<endl;
+        cin>>n;
+
+        if(!cin or n<2){
+            cout<<"errore"<<endl;
+            return -1;
+        }
+
+        cout<<endl;
+        cout<<a<<endl;
+        cout<<b<<endl;
+
+        for(i=0; i<n; i++){
+            sum=a+b;
+            cout<<sum<<endl;
+            a=b;
+            b=sum;
+        }
    return 0;
 }
